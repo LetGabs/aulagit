926a60469c8130641f521fe44d1bdbb0f61634fb
LetGabs <leticia.gabrielly@maisunifacisa.com.br>
2024-03-27 21:04:53 -0300

    Imprime mensagem

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,10 @@
 package org.example;
 
+import javax.swing.*;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        JOptionPane.showMessageDialog(null, "SAMUEL FALA COM A SUA BOYZINHA");
     }
+
 }
